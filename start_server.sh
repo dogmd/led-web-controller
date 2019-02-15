3b94ea18b0739451cfa1b939896484548ff79c31
dogmd <jacobsenscott07@gmail.com>
2019-02-15 05:18:13 -0600

    change start_server script to run server through websocket.py

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,2 @@
 /usr/bin/tmux kill-session -t websocket
-/usr/bin/tmux kill-session -t led-relay
 /usr/bin/tmux new-session -d -s websocket 'cd /home/pi/outdoor-lights/controller; python3.6 ./websocket.py'
-/usr/bin/tmux new-session -d -s led-relay 'cd /home/pi/outdoor-lights/controller/led_control/; python3.6 led_relay.py'
