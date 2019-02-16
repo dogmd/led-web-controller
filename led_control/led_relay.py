5f02416be50610a42723839a6b648ac8ad85ce6c
WScottJacobsen <jacobsenscott07@gmail.com>
2019-02-16 03:48:13 -0600

    Recover from corruption

--- led_control/led_relay.py
+++ led_control/led_relay.py
@@ -1,20 +1,23 @@
 import time
 import board
 import adafruit_dotstar as dotstar
-import led_control.effect_controller as ec
+import effect_controller as ec
 
-def main():
-	# Using a DotStar Digital LED Strip with 30 LEDs connected to digital pins
-	dots = dotstar.DotStar(board.D6, board.D5, 200, brightness=1, auto_write=False)
-	n_dots = len(dots)
-	effect = ec.EffectController(n_dots)
-	delay = 0.05
+# Using a DotStar Digital LED Strip with 30 LEDs connected to digital pins
+dots = dotstar.DotStar(board.D6, board.D5, 200, brightness=1, auto_write=False)
+n_dots = len(dots)
+effect = ec.EffectController(n_dots)
+delay = 0.05
+ticks = 0
 
-	while True:
-		effect.step()
-		dots = effect.pixels
-		dots.show()
-		time.sleep(delay)
-
-def update():
-	ec.import_settings()
+while True:
+	effect.step()
+	for ind in range(n_dots):
+		dots[ind] = effect.pixels[ind]
+	dots = effect.pixels
+	dots.show()
+	time.sleep(delay)
+	ticks += 1
+	if (ticks == 10):
+		effect.import_settings()
+		ticks = 0
