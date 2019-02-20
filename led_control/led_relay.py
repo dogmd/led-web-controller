833de3ebe3f5456b751125806444bbe6cbe128b3
dogmd <jacobsenscott07@gmail.com>
2019-02-20 03:21:54 -0600

    Change strip to use serial pins

--- led_control/led_relay.py
+++ led_control/led_relay.py
@@ -4,7 +4,7 @@ import adafruit_dotstar as dotstar
 import effect_controller as ec
 
 # Using a DotStar Digital LED Strip with 30 LEDs connected to digital pins
-dots = dotstar.DotStar(board.D6, board.D5, 200, brightness=1, auto_write=False)
+dots = dotstar.DotStar(board.SCK, board.MOSI, 200, brightness=1, auto_write=False)
 n_dots = len(dots)
 effect = ec.EffectController(n_dots)
 delay = 0.05
@@ -14,7 +14,6 @@ while True:
 	effect.step()
 	for ind in range(n_dots):
 		dots[ind] = effect.pixels[ind]
-	dots = effect.pixels
 	dots.show()
 	time.sleep(delay)
 	ticks += 1
