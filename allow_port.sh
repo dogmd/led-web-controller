b3bf57a4c02612fdec01f377ad7fd6b60652e898
WScottJacobsen <jacobsenscott07@gmail.com>
2019-11-18 14:47:21 -0600

    Setup rest file structure

--- allow_port.sh
+++
@@ -1,2 +0,0 @@
-iptables -A INPUT -i wlp2s0 -p tcp --dport 8765 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o wlp2s0 -p tcp --sport 8765 -m state --state ESTABLISHED -j ACCEPT
--- start_server.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/tmux kill-session -t websocket
-/usr/bin/tmux new-session -d -s websocket 'cd /home/pi/outdoor-lights/controller; python3.6 ./websocket.py'
